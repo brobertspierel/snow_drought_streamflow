06b5463669640975ec9dc02ba6296261fb047d4d
brobertspierel <42220725+brobertspierel@users.noreply.github.com>
2021-03-03 14:20:45 -0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,14 @@ release = '0.0.1'
 # ones.
 extensions = [
 ]
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
 
+# The master toctree document.
+master_doc = 'index'
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
